50432db51f1038a93b22bf92fa6d0966654daf18
Spencer Johnson <sljohnson2222@gmail.com>
2022-02-06 19:15:25 -0600

    Completed first 3 deliverables

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+// // As a user, I should be able to type a task into the input field.
+// As a user, I should be able to click some form of a submit button.
+
+// As a user, I expect to see the task string that I provided appear in the DOM after the submit button has been activated.
+
+//Need to us the preventDefault() with the submit function
+
+
+function renameFormId() {
+  let newName =  document.getElementById('new-task-description');
+  return newName.id = "new_task_description"
+}
+renameFormId()
+
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    buildToDo(e.target.new_task_description.value);
+    form.reset();
+  });
+
+  function buildToDo(toDo) {
+    let p = document.createElement('p');
+    let btn = document.createElement('button');
+    btn.addEventListener('click', handleDelete)
+    btn.textContent = "X"
+    p.textContent = `${toDo}  `;
+    p.appendChild(btn);
+    console.log(p)
+    document.querySelector('#list').appendChild(p)
+  }
+
+  function handleDelete(e) {
+    e.target.parentNode.remove();
+  }
+
 });
